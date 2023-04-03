73a1a785c3446058c4f48990c7c732cd444d9a7d
Bino <you@example.com>
2023-04-03 11:09:58 -0400

    done

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
+
 import express from 'express'
 import HelloController
     from "./controllers/hello-controller.js"
@@ -5,10 +12,11 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitController from "./controllers/tuits/tuits-controller.js"
 import cors from 'cors'
+
 const app = express()
 app.use(express.json());
 app.use(cors())
 TuitController(app)
 HelloController(app)
 UserController(app)
-app.listen(process.env.PORT ||4000)
+app.listen(process.env.PORT || 4000)
